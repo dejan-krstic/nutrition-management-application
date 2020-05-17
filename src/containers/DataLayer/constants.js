b7eef2be09df37949dae6f19fbe6cb363c054ba3
dejan-krstic <dejanmkrstic@gmail.com>
2020-05-17 22:56:27 +0200

    Changed Mocky links

--- src/containers/DataLayer/constants.js
+++ src/containers/DataLayer/constants.js
@@ -1,5 +1,5 @@
 export const RECIPES_URL =
-  "http://www.mocky.io/v2/5eb7ee1e2d00000f00357b57?mocky-delay=500ms";
+  "https://www.mocky.io/v2/5eb7ee1e2d00000f00357b57?mocky-delay=500ms";
 
-export const MEALS_URL = "http://www.mocky.io/v2/5eb802642d00005d00357ba6";
-export const GROCERIES_URL = "http://www.mocky.io/v2/5eb802fb2d00003700357ba7";
+export const MEALS_URL = "https://www.mocky.io/v2/5ec1aa842f00005a00c34da4";
+export const GROCERIES_URL = "https://www.mocky.io/v2/5eb802fb2d00003700357ba7";
